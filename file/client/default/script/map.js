e372f74d04fa94aa00d667282ebf51abdc84a3bd
kgowdhaman <kgowdhaman.deloitte.com>
2012-04-06 15:53:01 +0530

    updated

--- file/client/default/script/map.js
+++ file/client/default/script/map.js
@@ -23,41 +23,13 @@ var map = function () {
         // Keep the reference to the map object;
         self.map = res.map;
         // Map is being shown, lets populate it with data points
-        self.populateMap(lat, lon);
+        //self.populateMap(lat, lon);
       }, function (error) {
         // something seriously wrong here. Show error
         alert(error);
       });
-    },
-    
-    populateMap: function (lat, lon) {
-      $fh.act({
-        act: 'getPlacemarks',
-        req: {
-          lat: lat,
-          lon: lon
-        }
-      }, function (res) {
-        var points = res.points;
-              
-        // Iterate over the points array, adding each to the map
-        for (var pi=0,pl=points.length; pi<pl; pi++) {
-          var point = points[pi];
-          
-          // Create the marker, then add it to the map
-          var pos = new google.maps.LatLng(point.lat, point.lon);
-          
-          var marker = new google.maps.Marker({
-            position: pos,
-            map: self.map,
-            title: point.title
-          });
-        }   
-      }, function (code, errorprops, params) {
-        // something went wrong. Show error
-        alert(code);
-      });
     }
+    
   };
 
   return {
