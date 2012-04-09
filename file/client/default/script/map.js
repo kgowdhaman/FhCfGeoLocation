8425aa3bd18f07eb7edd6f954363e324c7641582
kgowdhaman <kgowdhaman.deloitte.com>
2012-04-09 15:09:47 +0530

    updated

--- file/client/default/script/map.js
+++ file/client/default/script/map.js
@@ -1,18 +1,16 @@
-//default co-ordinates
-var lat = 12.95312, lon = 77.699239;  
-
 var map = function () {
   var self = {
     
     // Show the map on screen
     show: function () {
-        $fh.geo( {interval:5000}, function(geoRes){
+      var lat = 12.95312,
+          lon = 77.699239; // default
+      $fh.geo( {interval:5000}, function(geoRes){
         // result contains Location of Deloitte USI Bangalore (Bengaluru), Karnataka, India
         // geo fix 77.699239 12.95312 (Longitude,Latitude of Bengaluru)
        lat = geoRes.lat;
        lon = geoRes.lon;
-      } );
-
+      } );    
       //Pass lat & lon into map api, initialising map at that point
       $fh.map({
         target: document.getElementById('maps_div'),
@@ -23,13 +21,41 @@ var map = function () {
         // Keep the reference to the map object;
         self.map = res.map;
         // Map is being shown, lets populate it with data points
-        //self.populateMap(lat, lon);
+        self.populateMap(lat, lon);
       }, function (error) {
         // something seriously wrong here. Show error
         alert(error);
       });
-    }
+    },
     
+    populateMap: function (lat, lon) {
+      $fh.act({
+        act: 'getPlacemarks',
+        req: {
+          lat: lat,
+          lon: lon
+        }
+      }, function (res) {
+        var points = res.points;
+              
+        // Iterate over the points array, adding each to the map
+        for (var pi=0,pl=points.length; pi<pl; pi++) {
+          var point = points[pi];
+          
+          // Create the marker, then add it to the map
+          var pos = new google.maps.LatLng(point.lat, point.lon);
+          
+          var marker = new google.maps.Marker({
+            position: pos,
+            map: self.map,
+            title: point.title
+          });
+        }   
+      }, function (code, errorprops, params) {
+        // something went wrong. Show error
+        alert(code);
+      });
+    }
   };
 
   return {
---
+++ file/cloud/main.js
@@ -0,0 +1,40 @@
+var util = require('util');
+/* main.js
+ * All calls here are publicly exposed as REST API endpoints.
+ * - all parameters must be passed in a single JSON paramater.
+ * - the return 'callback' method signature is 'callback (error, data)', where 'data' is a JSON object.
+*/
+
+/* 'getConfig' server side REST API method.
+ * Trivial example of pulling in a shared config file.
+ */
+exports.getConfig = function(params, callback) {
+  //console.log("In getConfig() call");
+  var cfg = require("config.js");
+  return callback(null, {config: cfg.config});
+};
+
+exports.getPlacemarks = function (params, callback) {
+  //console.log('in getPlacemarks');
+  
+  var lat = 'undefined' !== typeof params.lat ? params.lat : 12.95312,
+      lon = 'undefined' !== typeof params.lon ? params.lon : 77.699239;
+  
+  // Add the passed in location to a points array
+  var points = [{lat: lat, lon: lon, title: 'Deloitte USI Bengaluru'}];
+    
+  // Push some more closeby points onto the array
+  //points.push({lat: lat + 0.002, lon: lon - 0.002, title: 'Top Left'});
+  //points.push({lat: lat + 0.002, lon: lon, title: 'Top Middle'});
+  //points.push({lat: lat + 0.002, lon: lon + 0.002, title: 'Top Right'});
+  
+  //points.push({lat: lat, lon: lon - 0.002, title: 'Middle Left'});  
+  //points.push({lat: lat, lon: lon + 0.002, title: 'Middle Right'});
+  
+  
+  //points.push({lat: lat - 0.002, lon: lon - 0.002, title: 'Bottom Left'});
+  //points.push({lat: lat - 0.002, lon: lon, title: 'Bottom Middle'});
+  //points.push({lat: lat - 0.002, lon: lon + 0.002, title: 'Bottom Right'});
+  
+  return callback(null, {points: points});
+};
